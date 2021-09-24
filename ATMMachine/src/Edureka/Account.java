0e68f4a5ee92e4953e42e0d41257aad83cc08831
Keshani_Silva <keshani.17@itfac.mrt.ac.lk>
2021-09-24 16:21:05 +0530

    Add files via upload

---
+++ ATMMachine/src/Edureka/Account.java
@@ -0,0 +1,10 @@
+package Edureka;
+
+public class Account {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ATMMachine/src/Edureka/OptionMenu.java
@@ -0,0 +1,11 @@
+package Edureka;
+
+import java.io.IOException;
+public class OptionMenu extends Account {
+
+
+	
+	
+	
+	
+}
